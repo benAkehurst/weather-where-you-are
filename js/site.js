641128c9cd321196b279ee6268e849734891d1d2
mistabenjo <benakehurst@hotmail.com>
2017-01-17 22:15:27 +0200

    3 - added more switch cases
    and started working on symbols for weather

--- js/site.js
+++ js/site.js
@@ -25,14 +25,17 @@
               
               var arr = response;
               var temp = parseInt(arr.main.temp);
-              var conditions = arr.weather[0].description;
-              var conditionsCapitalLetter = capitalizeFirstLetter(conditions);
+              var conditionsDesc = arr.weather[0].description;
+              var weather = arr.weather[0].main;
+              var weatherCapitalLetter = capitalizeFirstLetter(weather);
+
+              backgroundColor(temp);
+              conditionsIcon(conditionsDesc);
 
               var tempToF = convertToF(temp);
 
               $("#spanTempC").text(temp + ascii(176) + "C");
               $("#spanTempF").text(tempToF + ascii(176) + "F");
-
 
               $("#buttonF").click(function() {
                 $("#spanTempF").show();
@@ -44,9 +47,7 @@
                 $("#spanTempC").show();
               });
 
-              $("#spanConditions").text(conditionsCapitalLetter);
-
-              backgroundColor(temp);
+              $("#spanConditions").text(weatherCapitalLetter);
 
             }
         }); //End of ajax call to get weather
@@ -59,22 +60,47 @@
 
   function backgroundColor(temp){
     switch (temp <= 100){
-      case temp < 0: document.body.style.backgroundColor = "#0049bf";
+      case temp < 0: document.body.style.backgroundColor = "##092b60";
         break;
-      case temp < 10: document.body.style.backgroundColor = "#4586ef";;
+      case temp < 5: document.body.style.backgroundColor = "#1a4b9b";;
         break;
-      case temp < 20: document.body.style.backgroundColor = "#76e8fc";;
+      case temp < 7: document.body.style.backgroundColor = "#4782e0";;
         break;
-      case temp < 30: document.body.style.backgroundColor = "#efe745";;
+      case temp < 10: document.body.style.backgroundColor = "#37a1e8";;
         break;
-      case temp < 40: document.body.style.backgroundColor = "#efae45";;
+      case temp < 13: document.body.style.backgroundColor = "#2dcde5";;
         break;
-      case temp < 50: document.body.style.backgroundColor = "#ef6445";;
+      case temp < 15: document.body.style.backgroundColor = "#bce23f";;
         break;
-      case temp < 60: document.body.style.backgroundColor = "#ff1e1e";;
+      case temp < 17: document.body.style.backgroundColor = "#cde23f";;
         break;
-      case temp < 99: document.body.style.backgroundColor = "#ff1e1e";;
+      case temp < 20: document.body.style.backgroundColor = "#e0e23f";;
         break;
+      case temp < 25: document.body.style.backgroundColor = "#e2cd3f";;
+        break;
+      case temp < 27: document.body.style.backgroundColor = "#e2ba3f";;
+        break;
+      case temp < 30: document.body.style.backgroundColor = "#dba136";;
+        break;
+      case temp < 33: document.body.style.backgroundColor = "#db8936";;
+        break;
+      case temp < 35: document.body.style.backgroundColor = "#db7836";;
+        break;
+      case temp < 40: document.body.style.backgroundColor = "#db6836";;
+        break;
+      case temp < 50: document.body.style.backgroundColor = "#db5d36";;
+        break;
+      case temp < 99: document.body.style.backgroundColor = "#db4736";;
+        break;
+    }
+  }
+
+  function conditionsIcon(conditionsDesc){
+
+    var sectionWeatherLogo = document.getElementById('sectionWeatherLogo');
+
+    if(conditionsDesc == 'clear sky'){
+      sectionWeatherLogo.innerHTML = "<img src='assets/images/sun.png'>";
     }
   }
     
